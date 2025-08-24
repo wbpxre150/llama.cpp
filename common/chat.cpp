ccad78f571fcc61e7592a1cfd796f6e9e6c099f2
Benjamin Oldenburg <benjamin.oldenburg@ordis.co.th>
2025-08-24 20:37:09 +0700

    Update common/chat.cpp
    Co-authored-by: Marcel de Vries <marceldev89@gmail.com>

--- common/chat.cpp
+++ common/chat.cpp
@@ -2121,7 +2121,7 @@ static common_chat_params common_chat_params_init_qwen3_coder_xml(const common_c
                         if (param_schema.contains("type")) {
                             std::string param_type = param_schema["type"];
                             if (param_type == "string") {
-                                param_rule += not_parameter_end + " ";
+                                param_rule += not_parameter_end;
                             } else {
                                 param_rule += builder.add_schema(name + "-parameter-" + param_name, param_schema);
                             }
