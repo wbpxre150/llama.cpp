dc6c4f2c2e446e30c0527a93b7efb00ffe8066b2
Benjamin Oldenburg <benjamin.oldenburg@ordis.co.th>
2025-08-24 17:03:57 +0700

    Update common/chat.cpp
    Co-authored-by: Kashyap Jois <kjois@iprdgroup.com>

--- common/chat.cpp
+++ common/chat.cpp
@@ -2156,6 +2156,9 @@ static common_chat_params common_chat_params_init_qwen3_coder_xml(const common_c
             "<parameter=",
             "</parameter>",
         };
+    } else {
+        // When no tools are provided, disable lazy grammar to avoid "no triggers set" error
+        data.grammar_lazy = false;
     }
 
     data.prompt = apply(tmpl, inputs);
